56f361764326d677b02e6cb4f3e548c805334103
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-02-27 03:03:27 +0600

    Entity commit: Crop and UserCrop

--- server/src/main/java/com/district12/backend/entities/UserCrop.java
+++ server/src/main/java/com/district12/backend/entities/UserCrop.java
@@ -1,4 +1,36 @@
 package com.district12.backend.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "user_crops")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class UserCrop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    @ToString.Exclude
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    @ToString.Exclude
+    private Crop crop;
+
+    @Column(nullable = false)
+    private ZonedDateTime selectedAt;
+
 }
