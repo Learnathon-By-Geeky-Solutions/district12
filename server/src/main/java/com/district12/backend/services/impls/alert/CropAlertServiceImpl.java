738389f5f2050c464f84f73dd57678afd29c9a1c
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-03-02 12:21:33 +0600

    Service commit: Alert subtypes(with abstraction)

---
+++ server/src/main/java/com/district12/backend/services/impls/alert/CropAlertServiceImpl.java
@@ -0,0 +1,10 @@
+package com.district12.backend.services.impls.alert;
+
+import com.district12.backend.services.abstractions.alert.CropAlertService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CropAlertServiceImpl implements CropAlertService {
+}
---
+++ server/src/main/java/com/district12/backend/services/impls/alert/WeatherAlertServiceImpl.java
@@ -0,0 +1,10 @@
+package com.district12.backend.services.impls.alert;
+
+import com.district12.backend.services.abstractions.alert.WeatherAlertService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WeatherAlertServiceImpl implements WeatherAlertService {
+}
