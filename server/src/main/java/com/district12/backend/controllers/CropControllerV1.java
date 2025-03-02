729d3df2ae3ae3b65bfc80c26ded7ee1c8a031d3
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-03-02 12:24:07 +0600

    Controller commit: For alert subtypes

--- server/src/main/java/com/district12/backend/controllers/CropControllerV1.java
+++ server/src/main/java/com/district12/backend/controllers/CropControllerV1.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/api/crops")
+@RequestMapping("/v1/crops")
 @RequiredArgsConstructor
 @Slf4j
 public class CropControllerV1 {
